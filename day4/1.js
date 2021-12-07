5459adb22dab1f31d64b799061914bae8c8b1411
Tales Ruan <talesruan@gmail.com>
2021-12-06 23:54:11 -0300

    Day 4 part 1.

---
+++ day4/1.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const data = fs.readFileSync('input.txt', 'utf8')
+console.time("Elapsed time")
+
+const bingoGridSize = 5;
+
+const fn = function () {
+	const parsedData = parseData(data);
+	const numbersDrawnSoFar = [];
+	for (const drawnNumber of parsedData.drawnNumbers) {
+		numbersDrawnSoFar.push(drawnNumber);
+		if (numbersDrawnSoFar.length < bingoGridSize) continue; // can't possibly win with < 5 numbers drawn
+		for (const board of parsedData.boards) {
+			if (!isBoardWon(board, numbersDrawnSoFar)) continue;
+			console.log("Found winning board.");
+			return calculateBoardScore(board, numbersDrawnSoFar, drawnNumber);
+		}
+	}
+};
+
+const parseData = (dataString) => {
+	const lines = dataString.split("\n");
+	const drawnNumbers = lines[0].split(",").map(n => parseInt(n));
+	const boards = [];
+	for (let lineNumber = 1; lineNumber < lines.length; lineNumber++) {
+		const line = lines[lineNumber];
+		if (line === "") continue;
+		const boardNumber = Math.trunc((lineNumber-1) / (bingoGridSize + 1));
+		const rowNumber = (lineNumber-2) % (bingoGridSize + 1);
+		const parsedRow = line.trim().split(/\s+/).map(n => parseInt(n));
+		if (rowNumber === 0) boards[boardNumber] = [];
+		boards[boardNumber][rowNumber] = parsedRow;
+	}
+	return {drawnNumbers, boards};
+};
+
+const isBoardWon = (board, drawnNumbers) => {
+	const hitMatrix = getHitMatrix(board, drawnNumbers); // I know it can be more efficient if I store the hit matrix and do not recalculate everytime
+	for (let a = 0; a < bingoGridSize; a++) {
+		let rowHit = true;
+		let columnHit = true;
+		for (let b = 0; b < bingoGridSize; b++) {
+			rowHit = rowHit && hitMatrix[a][b]
+			columnHit = columnHit && hitMatrix[b][a]
+		}
+		if (rowHit || columnHit) return true;
+	}
+	return false;
+};
+
+const calculateBoardScore = (board, drawnNumbers, lastNumberDrawn) => {
+	const hitMatrix = getHitMatrix(board, drawnNumbers);
+	let sumOfUnmarkedNumbers = 0;
+	for (let a = 0; a < bingoGridSize; a++) {
+		for (let b = 0; b < bingoGridSize; b++) {
+			if (!hitMatrix[a][b]) sumOfUnmarkedNumbers += board[a][b];
+		}
+	}
+	return sumOfUnmarkedNumbers * lastNumberDrawn
+}
+
+const getHitMatrix = (board, drawnNumbers) => {
+	const rows = [];
+	for (let rowNumber = 0; rowNumber < bingoGridSize; rowNumber++) {
+		const row = []
+		for (let colNumber = 0; colNumber < bingoGridSize; colNumber++) {
+			const isMatch = drawnNumbers.includes(board[rowNumber][colNumber]);
+			row.push(isMatch);
+		}
+		rows.push(row);
+	}
+	return rows;
+};
+
+console.log("Result: ", fn ());
+console.timeEnd('Elapsed time');
